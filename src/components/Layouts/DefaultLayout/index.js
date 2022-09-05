0b3d5a4434615ff1cd9d9e5d07438b43dbaaeb50
Danhhoa <hoathanhdanh1510@gmail.com>
2022-09-05 22:23:43 +0700

    feat: initial default layout

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,14 +1,18 @@
 import Header from '~/components/Layouts/components/Header';
 import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
   return (
-    <div>
-      <Header></Header>
-      <div className="container">
-        <Sidebar></Sidebar>
+    <div className={cx('wrapper')}>
+      <Header />
+      <div className={cx('container')}>
+        <Sidebar />
 
-        <div className="content"> {children} </div>
+        <div className={cx('content')}> {children} </div>
       </div>
     </div>
   );
